e774da04192a0fcd0c58695ad1c7dfadd83f43a2
smartboy0310 <qdustudent6@gmail.com>
2021-10-13 15:44:55 +0500

    Create render js

--- js/main.js
+++ js/main.js
@@ -20,42 +20,7 @@ function updateTodos() {
 
 updateTodos();
 
-function renderTodos(arr, node) {
-	node.innerHTML = null;
 
-	const allCount = todos.length;
-	const completedCount = todos.filter(
-		(row) => row.isCompleted === true,
-	).length;
-
-	elAllCount.textContent = allCount;
-	elCompletedCount.textContent = completedCount;
-	elUncompletedCount.textContent = allCount - completedCount;
-
-	arr.forEach((todo) => {
-		const newLi = document.createElement('li');
-		const newButton = document.createElement('button');
-		const newCheckbox = document.createElement('input');
-
-		newLi.textContent = todo.title;
-		newButton.textContent = 'Delete';
-
-		newCheckbox.type = 'checkbox';
-		newButton.classList.add('todo__delete-btn');
-		newCheckbox.classList.add('todo__checkbox');
-		newButton.dataset.todoId = todo.id;
-		newCheckbox.dataset.todoId = todo.id;
-
-		if (todo.isCompleted) {
-			newCheckbox.checked = true;
-			newLi.style.textDecoration = 'line-through';
-		}
-
-		newLi.appendChild(newCheckbox);
-		newLi.appendChild(newButton);
-		node.appendChild(newLi);
-	});
-}
 elTodoList.addEventListener('click', (evt) => {
 	if (evt.target.matches('.todo__delete-btn')) {
 		const todoId = Number(evt.target.dataset.todoId);
